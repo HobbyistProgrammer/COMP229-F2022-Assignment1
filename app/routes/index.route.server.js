867e8764118b9ce24b164ff94b7b6a956a212847
Benton Le <ble21@my.centennialcollege.ca>
2022-10-21 21:44:13 -0400

    small files

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,20 +1,17 @@
-// Index Routes
-import { Router } from 'express';
-import { displayHomePage, 
-    displayAboutPage, 
+import { Router } from "express";
+import { displayAboutPage, 
+    displayContactPage, 
+    displayHomePage, 
     displayProjectsPage, 
-    displayServicesPage, 
-    displayContactPage } from '../controllers/index.controller.server.js'
+    displayServicesPage } from "../controllers/index.controller.server.js";
 
-// Intanciating the router
 const router = Router();
 
-// add middleware to connect application
-router.get('/',displayHomePage);
-router.get('/home',displayHomePage);
-router.get('/about', displayAboutPage );
-router.get('/projects', displayProjectsPage );
-router.get('/services', displayServicesPage );
-router.get('/contact', displayContactPage );
+router.get('/', displayHomePage);
+router.get('/home', displayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/projects', displayProjectsPage);
+router.get('/services', displayServicesPage);
+router.get('/contact', displayContactPage);
 
 export default router;

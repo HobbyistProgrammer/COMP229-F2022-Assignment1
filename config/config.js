de932badad4376996170f19e49f8b76d56c2aa0d
Benton Le <ble21@my.centennialcollege.ca>
2022-10-21 20:46:02 -0400

    Completed Assignment 2

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://ble21:yBwWrej9A5xkrqNj@comp229.ldmsa0l.mongodb.net/media?retryWrites=true&w=majority"
